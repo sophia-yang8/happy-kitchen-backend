8f0a64250caea08c2743043c518060b48ec7bb5e
Sophia Yang <sophiayang822@gmail.com>
2025-04-12 16:54:16 -0400

    working

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://happy-kitchen-backend.vercel.app/"]}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://happy-kitchen-backend.vercel.app"]}})
 @app.route('/')
 def home():
     return jsonify({
